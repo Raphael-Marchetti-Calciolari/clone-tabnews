d472e6bde5c3e881468f06fdf98a99672dd4a271
@Raphael-Marchetti-Calciolari <rapha.marchetti@gmail.com>
2026-02-10 10:55:47 -0300

    Initial file

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+function Home() {
+    return <div>
+        <h1>Se você quer ficar rico, clica nesse botão</h1>
+        <button onClick={() => alert("Parabéns todo seu saldo bancário sumiu!")}>Clique aqui!</button>
+    </div>
+}
+
+export default Home;
